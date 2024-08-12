5fae2d52264bc52279c041dd085f38b2a82fa3a1
Mary Villan <marivillanueva@gmail.com>
2024-08-12 13:32:07 -0600

    app rutas publicas

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/user', userRoutes);
-app.use('/rutas', authenticateToken, routeRoutes);
+app.use('/rutas', routeRoutes);
 app.use('/boletos', authenticateToken, ticketRoutes);
 
 
