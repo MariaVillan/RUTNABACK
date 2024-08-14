d3aa8066f1c024fb4b3ce1b551b8f7a5d70dcf52
Mary Villan <marivillanueva@gmail.com>
2024-08-13 19:36:59 -0600

    eliminar y procesar boletos2

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -6,7 +6,7 @@ const authenticateToken = require('../middleware/auth');
 router.post('/comprar', authenticateToken, ticketController.comprarBoleto);
 router.get('/listar', ticketController.listarBoletos);
 router.get('/versaldo', authenticateToken, ticketController.verSaldo);
-router.post('/escanearBoleto', ticketController.buscarBoleto, ticketController.escanearBoleto);
+router.post('/escanearBoleto', authenticateToken, ticketController.buscarBoleto, ticketController.escanearBoleto);
 
 
 module.exports = router;
